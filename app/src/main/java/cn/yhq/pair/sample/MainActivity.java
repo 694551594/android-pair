90f35ad78ceff0d8cf0046d006efa9ed281d42f6
yanghuiqiang <694551594@qq.com>
2016-11-17 10:49:49 +0800

    重构。

--- app/src/main/java/cn/yhq/pair/sample/MainActivity.java
+++ app/src/main/java/cn/yhq/pair/sample/MainActivity.java
@@ -43,29 +43,16 @@ public class MainActivity extends BaseActivity {
         super.onViewCreated(savedInstanceState);
 
         mUser = new User();
-        mUser.password = "密码";
-        mUser.username = "用户名";
+        mUser.password = "123456";
+        mUser.username = "694551594";
 
         PairView pairView = this.getView(R.id.pairview);
 
         new Pair.Builder(this)
                 .addCatalog(
                         new PairCatalog()
-                                .setTitle("我是组标题")
+                                .setTitle("我是一组有图标的")
                                 .addItems(
-                                        new TextPairItem()
-                                                .setIcon(R.drawable.find_more_friend_photograph_icon)
-                                                .setText("哈哈")
-                                                .setKey("朋友圈")
-                                                .setDescription("我是描述")
-                                                .setAction(new PairActivityAction(this, MainActivity.class))
-                                                .addIntercept(new PairIntercept<TextPairItem>() {
-                                                    @Override
-                                                    public TextPairItem intercept(Chain<TextPairItem> chain) throws Exception {
-                                                        chain.getItem().setText("我是拦截器设置的文本");
-                                                        return chain.getItem();
-                                                    }
-                                                }),
                                         new TextPairItem()
                                                 .setIcon(R.drawable.find_more_friend_scan)
                                                 .setKey("扫一扫"),
@@ -74,45 +61,81 @@ public class MainActivity extends BaseActivity {
                                                 .setKey("朋友圈"),
                                         new TextPairItem()
                                                 .setIcon(R.drawable.find_more_friend_shake)
-                                                .setKey("摇一摇"),
+                                                .setKey("摇一摇")
+                                )
+                ).addCatalog(
+                    new PairCatalog()
+                            .setTitle("拦截器测试")
+                            .addItems(
+                                    new TextPairItem()
+                                            .setIcon(R.drawable.find_more_friend_photograph_icon)
+                                            .setText("朋友圈")
+                                            .setKey("拦截器设置的文本")
+                                            .setDescription("之前的文本为：朋友圈")
+                                            .addIntercept(new PairIntercept<TextPairItem>() {
+                                                @Override
+                                                public TextPairItem intercept(Chain<TextPairItem> chain) throws Exception {
+                                                    chain.getItem().setText("我是拦截器设置的文本");
+                                                    return chain.getItem();
+                                                }
+                                            })
+                            )
+                ).addCatalog(
+                    new PairCatalog()
+                            .setTitle("action测试")
+                            .addItems(
+                                    new TextPairItem()
+                                            .setText("对话框")
+                                            .setKey("对话框")
+                                            .setDescription("点我可以打开对话框")
+                                            .setAction(new PairDialogAction(this) {
+
+                                                @Override
+                                                protected IDialog onCreateDialog(Context context) {
+                                                    return DialogBuilder.messageDialog(context).setMessage("我是对话框哦").create();
+                                                }
+                                            }),
+                                    new TextPairItem()
+                                            .setText("进入新的activity")
+                                            .setKey("activity")
+                                            .setAction(new PairActivityAction(this, MainActivity.class))
+                                )
+                ).addCatalog(
+                        new PairCatalog()
+                                .setTitle("Preference测试")
+                                .addItems(
+                                        new SwitchPairItem()
+                                                .setKey("测试Preference")
+                                                .setAction(new PairPreferenceAction(this, "test")),
+                                        new SwitchPairItem().setChecked(true).setKey("哈哈")
+                                )
+                ).addCatalog(
+                        new PairCatalog()
+                                .setTitle("没图标的")
+                                .addItems(
                                         new TextPairItem()
                                                 .setKey("没有图标")
                                                 .setText("没有图标的文本哦"),
                                         new TextPairItem()
                                                 .setKey("这是一个日期格式化的例子")
                                                 .setText(System.currentTimeMillis())
-                                                .addIntercept(new DateFormatIntercept<>()),
-                                        new SwitchPairItem()
-                                                .setKey("测试Preference")
-                                                .setAction(new PairPreferenceAction(this, "test")),
-                                        new TextPairItem()
-                                                .setText("对话框")
-                                                .setKey("对话框")
-                                                .setDescription("点我可以打开对话框")
-                                                .setAction(new PairDialogAction(this) {
-
-                                                    @Override
-                                                    protected IDialog onCreateDialog(Context context) {
-                                                        return DialogBuilder.messageDialog(context).setMessage("我是对话框哦").create();
-                                                    }
-                                                }),
+                                                .addIntercept(new DateFormatIntercept<>())
+                                )
+                ).addCatalog(
+                        new PairCatalog()
+                                .setTitle("这是一个数据绑定的例子")
+                                .addItems(
                                         new FieldPairItem()
-                                                .setKey("用户名哦")
+                                                .setKey("用户名")
                                                 .setEntity(mUser)
                                                 .setExp("${user.username}"),
                                         new FieldPairItem()
                                                 .setExp("${user.password}")
                                                 .setEntity(mUser)
-                                                .setKey("密码哦")
-                                        )
-                )
-                .addCatalog(
-                        new PairCatalog()
-                                .setTitle("我也是组标题")
-                                .addItems(
-                                        new SwitchPairItem().setChecked(true).setKey("哈哈")
+                                                .setKey("密码")
                                 )
-                ).build().setup(pairView);
+                )
+                .build().setup(pairView);
     }
 
     @Override
